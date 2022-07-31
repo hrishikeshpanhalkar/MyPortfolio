fbaa5279ed2a7cf58c162bcc1119004efe87234c
hrishikeshpanhalkar <hrishikeshpanhalkar7@gmail.com>
2022-07-31 11:50:15 +0530

    added code

---
+++ home/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
+from home.models import Contact
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    #Handle the form here:
+    if request.method == "POST":
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        desc = request.POST['desc']
+        c = Contact(name=name, phone=phone, email=email, desc= desc)
+        c.save()
+    return render(request, 'contact.html')
